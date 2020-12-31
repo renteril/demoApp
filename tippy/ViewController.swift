88c389a633893ce0aca110e3a9c8ff78a2e1dd8a
renteril <renteril@oregonstate.edu>
2020-12-30 16:49:12 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Luis Renteria on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
